f2c6d816003046f0630613f5c33c373d00eb03f6
Matt Bimpson <matt@Matts-MacBook-Pro.local>
2020-11-07 13:10:08 -0500

    uuid extension

--- src/database/migrations/1604767453599-init.ts
+++ src/database/migrations/1604767453599-init.ts
@@ -3,8 +3,9 @@ import {MigrationInterface, QueryRunner} from "typeorm";
 export class init1604767453599 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`);
         await queryRunner.query(
-            `CREATE TABLE "item" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL default now(), "description" character varying NOT NULL, "name" character varying NOT NULL)`
+            `CREATE TABLE "item" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL default now(), "description" character varying NOT NULL, "name" character varying NOT NULL, CONSTRAINT "PK_295c0898cceea20ac8ee103d98d" PRIMARY KEY ("id"))`
         )
     }
 
